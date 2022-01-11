a54b1998fbba27f780ca8514c97ad4149cf4405d
Marcus Yong <marcusyqy@gmail.com>
2022-01-12 02:56:38 +0800

    added todo and tried to get media-query

--- src/components/nav-menu.svelte
+++ src/components/nav-menu.svelte
@@ -1,76 +1,90 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import Boop from '../animations/boop.svelte';
-    import Hamburger from './hamburger-menu.svelte';
+	import Hamburger from './hamburger-menu.svelte';
+	import MediaQuery from '../helpers/media-query.svelte';
 
-    let showMobileMenu: boolean = false;
-    let openMobileMenu : boolean = false;
+	let showNormalMenu: boolean = false;
+	let openMobileMenu: boolean = false;
 
-    // Mobile menu click event handler
-    const handleMobileIconClick = () => {openMobileMenu = !openMobileMenu};
+	// Mobile menu click event handler
+	const handleMobileIconClick = () => {
+		openMobileMenu = !openMobileMenu;
+	};
 
-    // Media match query handler
-    const mediaQueryHandler = (e : MediaQueryListEvent) => {
-        // Reset mobile state
-        if (!e.matches) {
-            showMobileMenu = true;
-            openMobileMenu = false;
-        } else {
-            showMobileMenu = false;
-        }
-    };
+	// Media match query handler
+	const mediaQueryHandler = (e: MediaQueryListEvent) => {
+		// Reset mobile state
+		if (!e.matches) {
+			showNormalMenu = true;
+			openMobileMenu = false;
+		} else {
+			showNormalMenu = false;
+		}
+	};
 
-    onMount(() => {
-        const mediaListener = window.matchMedia('(max-width: 767px)');
-        mediaListener.addListener(mediaQueryHandler);
-    });
-
+	onMount(() => {
+		const mediaListener = window.matchMedia('(max-width: 767px)');
+		mediaListener.addListener(mediaQueryHandler);
+	});
 </script>
 
 <div>
-    {#if showMobileMenu}
-        <ul class="nav-menu">
-            <li class="letters">
-                <Boop boopParams={{ scale: 1.1, timing: 200 }}><a href="/about"><b>About Me</b></a></Boop>
-            </li>
-            <li class="letters">
-                <Boop boopParams={{ scale: 1.1, timing: 200 }}><a href="/work"><b>Work</b></a></Boop>
-            </li>
-            <li class="letters">
-                <Boop boopParams={{ scale: 1.1, timing: 200 }}><a href="/blog"><b>Blog</b></a></Boop>
-            </li>
-            <li class="letters">
-                <Boop boopParams={{ scale: 1.1, timing: 200 }}
-                    ><a href="/contact-me"><b>Contact</b></a></Boop
-                >
-            </li>
-        </ul>
-    {:else}
-        <Hamburger bind:open={openMobileMenu}>
-            <a style="padding-top:50px;font-size=1rem;" on:click={handleMobileIconClick} href="/about"><b>About Me</b></a>
-            <a style="padding-top:50px;font-size=1rem;" on:click={handleMobileIconClick} href="/work"><b>Work</b></a>
-            <a style="padding-top:50px;font-size=1rem;" on:click={handleMobileIconClick} href="/blog"><b>Blog</b></a>
-            <a style="padding-top:50px;font-size=1rem;" on:click={handleMobileIconClick} href="/contact-me"><b>Contact</b></a>
-        </Hamburger>
-    {/if}
+	<MediaQuery query="(max-width: 767px)" let:matches>
+		{#if matches}
+			<ul class="nav-menu">
+				<li class="letters">
+					<Boop boopParams={{ scale: 1.1, timing: 200 }}><a href="/about"><b>About Me</b></a></Boop>
+				</li>
+				<li class="letters">
+					<Boop boopParams={{ scale: 1.1, timing: 200 }}><a href="/work"><b>Work</b></a></Boop>
+				</li>
+				<li class="letters">
+					<Boop boopParams={{ scale: 1.1, timing: 200 }}><a href="/blog"><b>Blog</b></a></Boop>
+				</li>
+				<li class="letters">
+					<Boop boopParams={{ scale: 1.1, timing: 200 }}
+						><a href="/contact-me"><b>Contact</b></a></Boop
+					>
+				</li>
+			</ul>
+		{:else}
+			<Hamburger bind:open={openMobileMenu}>
+				<a style="padding-top:50px;font-size=1rem;" on:click={handleMobileIconClick} href="/about"
+					><b>About Me</b></a
+				>
+				<a style="padding-top:50px;font-size=1rem;" on:click={handleMobileIconClick} href="/work"
+					><b>Work</b></a
+				>
+				<a style="padding-top:50px;font-size=1rem;" on:click={handleMobileIconClick} href="/blog"
+					><b>Blog</b></a
+				>
+				<a
+					style="padding-top:50px;font-size=1rem;"
+					on:click={handleMobileIconClick}
+					href="/contact-me"><b>Contact</b></a
+				>
+			</Hamburger>
+		{/if}
+	</MediaQuery>
 </div>
 
 <style>
-    .nav-menu {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-    }
+	.nav-menu {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+	}
 
-    .bar {
-        display: block;
-        width: 25px;
-        height: 3px;
-        margin: 5px auto;
-        -webkit-transition: all 0.3s ease-in-out;
-        transition: all 0.3s ease-in-out;
-        background-color: #101010;
-    }
+	.bar {
+		display: block;
+		width: 25px;
+		height: 3px;
+		margin: 5px auto;
+		-webkit-transition: all 0.3s ease-in-out;
+		transition: all 0.3s ease-in-out;
+		background-color: #101010;
+	}
 
 	ul {
 		list-style-type: none;
@@ -87,7 +101,7 @@
 	}
 
 	.letters {
-        margin-left: 2rem;
+		margin-left: 2rem;
 		/* padding: 0.4rem; */
 		/* padding-right: 2rem; */
 		color: white;
@@ -105,24 +119,23 @@
 		color: var(--teal);
 	}
 
-    @media only screen and (max-width: 768px) {
-    /* .nav-menu { */
-    /*     position: fixed; */
-    /*     left: -100%; */
-    /*     top: 5rem; */
-    /*     flex-direction: column; */
-    /*     background-color: #fff; */
-    /*     width: 100%; */
-    /*     border-radius: 10px; */
-    /*     text-align: center; */
-    /*     transition: 0.3s; */
-    /*     box-shadow: */
-    /*         0 10px 27px rgba(0, 0, 0, 0.05); */
-    /* } */
+	@media only screen and (max-width: 768px) {
+		/* .nav-menu { */
+		/*     position: fixed; */
+		/*     left: -100%; */
+		/*     top: 5rem; */
+		/*     flex-direction: column; */
+		/*     background-color: #fff; */
+		/*     width: 100%; */
+		/*     border-radius: 10px; */
+		/*     text-align: center; */
+		/*     transition: 0.3s; */
+		/*     box-shadow: */
+		/*         0 10px 27px rgba(0, 0, 0, 0.05); */
+		/* } */
 
-    .letters {
-        margin: 2.5rem 0;
-    }
-}
+		.letters {
+			margin: 2.5rem 0;
+		}
+	}
 </style>
-
