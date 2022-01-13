ce98d2d0111837a94aabcd2f9e800a95e37f8261
Marcus Yong <marcusyqy@gmail.com>
2022-01-14 00:37:47 +0800

    simple-icons port(?)
    removed icons
    
    removed skill icon

---
+++ src/components/skill-icon.svelte
@@ -0,0 +1,54 @@
+<script lang="ts">
+    import Cpp from '../components/logos/cpp.svelte'
+    export let percentage :number;
+</script>
+
+<div class="border">
+
+    <div class="progress">
+        <progress style="color:var(--teal);"min={0}, max={100} value={percentage}></progress>
+    </div>
+</div>
+
+
+<style>
+
+    .border {
+        display:flex;
+        border-style:solid;
+        border-color:var(--teal);
+        border-radius:20px;
+        padding:10px;
+        flex-direction:column;
+        justify-content:center;
+        text-align:center;
+    }
+
+
+    .progress {
+        padding-left:2rem;
+        padding-right:2rem;
+        width:50%;
+        vertical-align: middle;
+    }
+
+    progress {
+        color:var(--teal);
+        border:none;
+        background:var(--grey);
+        border-radius:20px;
+        text-align:center;
+    }
+
+    progress::-moz-progress-bar {
+        background:var(--teal);
+    }
+
+    progress::-webkit-progress-value {
+        background:var(--grey);
+    }
+
+    progress::-webkit-progress-bar {
+        background:var(--teal);
+    }
+</style>
--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,11 +1,28 @@
 <script lang="ts">
-    import {slide} from 'svelte/transition'
+	import { slide } from 'svelte/transition';
 </script>
 
 <svelte:head>
-    <title>
-        About Marcus
-    </title>
+	<title>About Marcus</title>
 </svelte:head>
-<h1 transition:slide>About Page</h1>
+<div transition:slide class="box">
+	<h1 transition:slide>About Me</h1>
+	<div class="box" style="flex-direction:row;">
+		<p>
+			I'm an aspiring
+			<span style="color:var(--teal); font-weight:bold">software engineer</span>
+		</p>
+		<div class="box">
+		</div>
+	</div>
+</div>
 
+<style>
+	.box {
+		display: flex;
+		flex-direction: column;
+		margin: auto;
+		align-items: center;
+		justify-content: center;
+	}
+</style>
