c5cb1710118b10361caa0fb864cdd6964201029b
marcusyqy <marcusyqy@gmail.com>
2022-01-19 17:20:04 +0000

    Deploying to gh-pages from @ marcusyqy/marcusyqy.github.io@3ee33ecb6e5941cc8f553fd46ed04d5f3ca21a80 🚀

---
+++ _app/pages/about.svelte-c9dba91a.js
@@ -0,0 +1,3 @@
+import{S as h,i as f,s as m,j as p,e as b,t as x,_ as v,d as l,l as A,c as y,a as P,g as j,f as i,P as k,Q as q,T as E,U as d,$ as u}from"../chunks/vendor-ac50cdec.js";function S(_){let s,t,r,a,n;return{c(){s=p(),t=b("h1"),r=x("About Page"),this.h()},l(e){v('[data-svelte="svelte-3r8xsz"]',document.head).forEach(l),s=A(e),t=y(e,"H1",{});var c=P(t);r=j(c,"About Page"),c.forEach(l),this.h()},h(){document.title=`
+        About Marcus
+    `},m(e,o){i(e,s,o),i(e,t,o),k(t,r),n=!0},p:q,i(e){n||(E(()=>{a||(a=d(t,u,{},!0)),a.run(1)}),n=!0)},o(e){a||(a=d(t,u,{},!1)),a.run(0),n=!1},d(e){e&&l(s),e&&l(t),e&&a&&a.end()}}}class z extends h{constructor(s){super();f(this,s,null,S,m,{})}}export{z as default};
