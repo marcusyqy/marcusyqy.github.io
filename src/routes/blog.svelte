972f45a2fcbb1f7e020189c1b5afb29b5e59e093
Marcus Yong <marcusyqy@gmail.com>
2022-02-02 03:59:15 +0800

    removed all the newline shinanigans

--- src/routes/blog.svelte
+++ src/routes/blog.svelte
@@ -16,19 +16,20 @@
 </svelte:head>
 
 <div class="w-1/2 flex flex-col m-auto items-center justify-center text-center">
-    <h1 class="m-1 text-4xl font-big">Marcus Blog</h1>
+	<h1 class="m-1 text-4xl font-big">Marcus Blog</h1>
 	<p class="text-base font-thin p-0 font-small underline">Tech · Life · Food</p>
 	<div>
-        <p class="text-left underline font-semibold text-base"><span class="text-teal">{posts.length}</span> posts.</p>
+		<p class="text-left underline font-semibold text-base">
+			<span class="text-teal">{posts.length}</span> posts.
+		</p>
 		{#each posts as post}
 			<a href={`/posts/${post.url}`}>
-                <div class="m-2 p-2 border-teal border-b-2 border-dashed hover:border-2 rounded-lg">
-                    <h2 class="text-xl text-teal text-underline font-bold">{post.metadata.title}</h2>
-                    <p class="text-xs font-small">{post.metadata.date}</p>
-                    <p class="text-sm font-small italic">{post.metadata.description}</p>
-                </div>
+				<div class="m-2 p-2 border-teal border-b-2 border-dashed hover:border-2 rounded-lg">
+					<h2 class="text-xl text-teal text-underline font-bold">{post.metadata.title}</h2>
+					<p class="text-xs font-small">{post.metadata.date}</p>
+					<p class="text-sm font-small italic">{post.metadata.description}</p>
+				</div>
 			</a>
 		{/each}
 	</div>
 </div>
-
--- src/routes/posts/[post].svelte
+++ src/routes/posts/[post].svelte
@@ -17,34 +17,34 @@
 		{post.metadata.title} <span style="font-size:1rem">({post.metadata.date})</span>
 	</h1>
 	<!--show the post html with @html-->
-    <div class="dummy p-4">
-	{@html post.content}
-    </div>
+	<div class="dummy p-4">
+		{@html post.content}
+	</div>
 </div>
 
 <style>
-    .dummy :global(h1) {
-        @apply pt-3 pb-2 font-bold underline text-justify text-2xl font-big;
-    }
-    .dummy :global(h2) {
-        @apply pt-2 pb-1 font-bold text-justify text-teal text-xl font-small;
-    }
-    .dummy :global(h3) {
-        @apply pt-1 pb-1 underline text-justify text-lg font-small;
-    }
-    .dummy :global(pre) {
-        @apply p-4 text-justify font-small items-center justify-center;
-    }
-    .dummy :global(ul) {
-        @apply m-4;
-    }
-    .dummy :global(li) {
-        @apply list-disc;
-    }
-    .dummy :global(p) {
-        @apply text-justify text-base font-big;
-    }
-    .dummy :global(code) {
-        @apply font-bold font-small;
-    }
+	.dummy :global(h1) {
+		@apply pt-3 pb-2 font-bold underline text-justify text-2xl font-big;
+	}
+	.dummy :global(h2) {
+		@apply pt-2 pb-1 font-bold text-justify text-teal text-xl font-small;
+	}
+	.dummy :global(h3) {
+		@apply pt-1 pb-1 underline text-justify text-lg font-small;
+	}
+	.dummy :global(pre) {
+		@apply p-4 text-justify font-small items-center justify-center;
+	}
+	.dummy :global(ul) {
+		@apply m-4;
+	}
+	.dummy :global(li) {
+		@apply list-disc;
+	}
+	.dummy :global(p) {
+		@apply text-justify text-base font-big;
+	}
+	.dummy :global(code) {
+		@apply font-bold font-small;
+	}
 </style>
